b150fd0748414560327a86f6bbb111f39895ed62
cemquo <150678182+cemquo@users.noreply.github.com>
2024-05-21 12:00:28 +0200

    add

---
+++ src/main/MainChargeurCD.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.io.IOException;
+
+import donnees.CD;
+import XML.ChargeurCD;
+
+/**
+ * un main permettant de charger un CD
+ */
+public class MainChargeurCD {
+
+	/**
+	 * methode principale qui charge un cd
+	 * 
+	 * @param args
+	 *            inutilise
+	 * @throws IOException
+	 *             si fichier inexistant
+	 */
+	public static void main(String args[]) throws IOException {
+		String nomFichier = "src/musicbrainzSimple/Benabar_Benabar.xml";
+		ChargeurCD charge = new ChargeurCD(nomFichier);
+		CD c = charge.chargerCD();
+		System.out.println(c);
+	}
+
+}
